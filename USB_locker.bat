3f439819a9ebcd27a052ee75ccf7bb3b9b35a64a
Katsugami <152559845+Katsugami@users.noreply.github.com>
2024-07-23 22:50:27 +0200

    Update USB_locker.bat
    Ajoute de l'enregistrement du choix de la langue

--- USB_locker.bat
+++ USB_locker.bat
@@ -2,13 +2,20 @@
 setlocal enabledelayedexpansion
 
 :: Chemin du fichier de configuration
-set "config_file=%~dp0config.txt"
+set "config_file=%~dp0config.ini"
 
 :: Lire le fichier de configuration s'il existe
 if exist "%config_file%" (
-    for /f "tokens=1,2 delims==" %%a in (%config_file%) do (
-        if "%%a"=="lang" set lang=%%b
+    for /f "tokens=1* delims==" %%a in (%config_file%) do (
+        set "key=%%a"
+        set "value=%%b"
+        :: Supprimer les espaces de début et de fin pour la clé
+        for /f "tokens=*" %%i in ("!key!") do set "key=%%i"
+        :: Supprimer les espaces de début et de fin pour la valeur
+        for /f "tokens=*" %%j in ("!value!") do set "value=%%j"
+        if "!key!"=="lang" set "lang=!value!"
     )
+    set "lang=!lang:~0,2!"
 )
 
 :: Demande de la langue si non définie dans le fichier de configuration
@@ -38,6 +45,9 @@ if "%lang%"=="en" (
 ) else if "%lang%"=="fr" (
     set file=lang\USB_locker_FR.bat
     set errmsg=Erreur : USB_locker_FR.bat est introuvable dans le repertoire lang.
+) else (
+    echo Invalid choice / Choix invalide
+    echo %lang%
 )
 
 :: Verification du fichier et lancement
