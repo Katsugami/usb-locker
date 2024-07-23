895eb8d9c43244577f55321001b8f004555cc313
Katsugami <152559845+Katsugami@users.noreply.github.com>
2024-07-23 22:29:32 +0200

    Update USB_locker.bat

--- USB_locker.bat
+++ USB_locker.bat
@@ -1,21 +1,43 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-:: Demande de la langue
-echo Select your language / Choisissez votre langue:
-echo [1] English
-echo [2] Francais
-set /P lang=Enter the number corresponding to your choice / Entrez le numero correspondant a votre choix: 
+:: Chemin du fichier de configuration
+set "config_file=%~dp0config.txt"
 
-if "%lang%"=="1" (
+:: Lire le fichier de configuration s'il existe
+if exist "%config_file%" (
+    for /f "tokens=1,2 delims==" %%a in (%config_file%) do (
+        if "%%a"=="lang" set lang=%%b
+    )
+)
+
+:: Demande de la langue si non définie dans le fichier de configuration
+if not defined lang (
+    echo Select your language / Choisissez votre langue:
+    echo [1] English
+    echo [2] Francais
+    set /P lang=Enter the number corresponding to your choice / Entrez le numero correspondant a votre choix: 
+
+    if "!lang!"=="1" (
+        set lang=en
+    ) else if "!lang!"=="2" (
+        set lang=fr
+    ) else (
+        echo Invalid choice / Choix invalide
+        goto eof
+    )
+    echo !lang!
+    :: Sauvegarder la langue choisie dans le fichier de configuration
+    echo lang=!lang! > "%config_file%"
+)
+
+:: Définir le fichier et le message d'erreur en fonction de la langue
+if "%lang%"=="en" (
     set file=lang\USB_locker_EN.bat
     set errmsg=Error: USB_locker_EN.bat not found in the lang directory.
-) else if "%lang%"=="2" (
+) else if "%lang%"=="fr" (
     set file=lang\USB_locker_FR.bat
     set errmsg=Erreur : USB_locker_FR.bat est introuvable dans le repertoire lang.
-) else (
-    echo Invalid choice / Choix invalide
-    goto :eof
 )
 
 :: Verification du fichier et lancement
