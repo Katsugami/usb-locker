5c22a473971a2fe73908c37998647efd3663b8bd
Katsugami <152559845+Katsugami@users.noreply.github.com>
2025-06-23 22:23:35 +0200

    Update USB_locker_FR.bat
    Ajouter le changement dans le registre pour ne plus autoriser les clés USB, car il y a des clés USB qui, même avec l'interdiction des droits Windows, pouvaient quand même y accéder.

--- lang/USB_locker_FR.bat
+++ lang/USB_locker_FR.bat
@@ -1,16 +1,17 @@
 :: USB Locker v1.4
 :: Auteur: g4xyk00
 :: Traducteur fr: Katsugami
-:: Teste sur Windows 7, 10, 11
+:: Teste sur Windows 11
 
 echo off
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a")
 
 :: Pour obtenir le SID actuel
 For /f "tokens=2 delims=\" %%a in ('whoami') do (set currentUser=%%a)
-wmic useraccount where name="%currentUser%" get sid | findstr "S-" > 0.txt
+powershell useraccount where name="%currentUser%" get sid | findstr "S-" > 0.txt
 set /P currentSID=<0.txt
 For /f "tokens=1 delims= " %%a in ('echo %currentSID%') do (set currentSID=%%a)
+For /F "tokens=3" %%c in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start 2^>nul') do (set StartVal=%%c)
  
 :ACTIVITE_PRINCIPALE
 cls
@@ -144,6 +145,14 @@ set /A AccessStatusCurrent = %AllClassesDenyStatusCurrent% + %RemovableDenyReadS
 echo L'acces actuel aux peripheriques de stockage amovible (Utilisateur actuel) est:
 IF "%AccessStatusCurrent%" NEQ "0" ( call :PainText 02 "REFUSE" )
 IF "%AccessStatusCurrent%" EQU "0" ( call :PainText 04 "AUTORISE" )
+echo L'acces aux Cles USB est :
+if "%StartVal%"=="0x3" (
+    call :PainText 04 "AUTORISE"
+) else if "%StartVal%"=="0x4" (
+    call :PainText 02 "REFUSE"
+) else (
+    echo La valeur Start est inconnue ou absente : %StartVal%
+)
 del 0.txt
 del 1.txt
 
@@ -155,6 +164,8 @@ echo [1] Autoriser l'acces aux peripheriques de stockage amovibles
 echo [2] Refuser l'acces aux peripheriques de stockage amovibles 
 echo [3] Revenir aux parametres par defaut
 echo [4] Creer un journal
+echo [5] Active les Cles USB
+echo [6] Desactive les Cles USB
 echo [0] Quitter le programme
 @echo:
 SET /P A=Veuillez selectionner une action (par ex. 2) et appuyez sur ENTRER: 
@@ -164,6 +175,8 @@ IF %A%==1 GOTO ACCESS_ALLOW
 IF %A%==2 GOTO ACCESS_DENY
 IF %A%==3 GOTO ACCESS_CLEAR
 IF %A%==4 GOTO CREATE_LOG
+IF %A%==5 GOTO ENABLE_USB_KEYS
+IF %A%==6 GOTO DISABLE_USB_KEYS
 
 :ACCESS_ALLOW
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices /t REG_DWORD /v Deny_All /d 0 /f > nul 2>&1
@@ -214,13 +227,25 @@ echo Revenu aux parametres par defaut!
 @echo:
 GOTO ACTIVITE_PRINCIPALE
 
+:ENABLE_USB_KEYS
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start /t REG_DWORD /d 3 /f
+echo L'acces aux Cles USB est maintenant AUTORISE!
+set "StartVal=0x3"
+@echo:
+GOTO ACTIVITE_PRINCIPALE
+
+:DISABLE_USB_KEYS
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start /t REG_DWORD /d 4 /f
+set "StartVal=0x4"
+echo L'acces aux Cles USB est maintenant REFUSE!
+@echo:
+GOTO ACTIVITE_PRINCIPALE
 
 :CREATE_LOG
 reg query HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices /s > usblock_log.txt
 reg query HKU\%currentSID%\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices >> usblock_log.txt
 @echo:
 GOTO ACTIVITE_PRINCIPALE
-
 
 :PainText
 <nul set /p "=%DEL%" > "%~2"
